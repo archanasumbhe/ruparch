c3482b242fd4a967ce805ac2b4a99990bb95586a
Rupali Vide <rupali.vide@tradofina.com>
2025-01-28 19:05:37 +0530

    product cart design

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,12 +3,15 @@ import { useRoutes } from "react-router-dom";
 import ProductList from "../pages/ProductList/ProductList";
 import Home from "../pages/Home/home";
 import ProductDetails from "../pages/ProductDetails/ProductDetails";
+import ProductCart from "../pages/ProductCart";
 
 const router: React.FC = () => {
   const routes = [
     { path: "/", element: <Home /> },
     { path: "/productList/:category" , element: <ProductList /> },
     { path: "/productDetails" , element: <ProductDetails /> },
+    { path: "/cart" , element: <ProductCart/> },
+
   ];
 
   const element = useRoutes(routes);
